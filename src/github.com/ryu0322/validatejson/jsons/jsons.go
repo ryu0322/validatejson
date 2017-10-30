a016cd8b96ff3659c68bd14292f937e62b71150e
ryuji.urata <ryuji.urata@sound-f.jp>
2017-10-30 16:09:55 +0900

    コミット

--- src/github.com/ryu0322/validatejson/jsons/jsons.go
+++ src/github.com/ryu0322/validatejson/jsons/jsons.go
@@ -1,4 +1,4 @@
-package Jsons
+package jsons
 
 import (
 	"io/ioutil"
--- src/github.com/ryu0322/validatejson/main.go
+++ src/github.com/ryu0322/validatejson/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/ryu0322/validatejson/reader"
 )
 
 func main() {
@@ -10,4 +12,8 @@ func main() {
 		fmt.Printf("引数が不足しています。読み込みファイルを指定してください。\n")
 		return
 	}
+
+	rows := reader.Reader(os.Args[1])
+
+	maps := jsons.CreateActionGroup(*rows)
 }
